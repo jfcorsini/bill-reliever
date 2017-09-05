284894aa420ec67a3414b4587a60854536adfb09
Joao Fernando Corsini <jf.corsini@gmail.com>
2017-09-05 00:29:16 -0300

    Adds 'My Profile' to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,6 +68,9 @@
 
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
+                                        <a href="/user/{{Auth::user()->id}}">My profile</a>
+                                    </li>
+                                    <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
